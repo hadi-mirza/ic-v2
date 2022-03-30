3d2d504e1eaf8b8fd6fced77747680e1ba154a93
hadi-mirza <hadimirza1@gmail.com>
2022-03-29 21:42:37 -0400

    add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,70 +1,72 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { isBoolean, mapValues, uniq } from 'lodash';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
-  title = 'angular-crash';
-  lib = { categories: [
-    'Performance', 'Investments', 'Operations' ],
-    applets: [ {
-    name: 'Performance Snapshot',
-    categories: ['Performance'] },
-    {
-    name: 'Commitment Widget', categories: ['Investments']
-    }, {
-    name: 'CMS',
-    categories: ['Investments', 'Performance' ] }
-    ] };
+export class AppComponent implements OnInit {
+  lib = {
+    categories: ['Performance', 'Investments', 'Operations'],
+    applets: [
+      {
+        name: 'Performance Snapshot',
+        categories: ['Performance'],
+      },
+      {
+        name: 'Commitment Widget',
+        categories: ['Investments'],
+      },
+      {
+        name: 'CMS',
+        categories: ['Investments', 'Performance'],
+      },
+    ],
+  };
 
-    // For transforming the model, create a new model within the getModel() function: https://opensource.com/article/20/5/data-modeling-javascript
+  // For transforming the model, create a new model within the getModel() function: https://opensource.com/article/20/5/data-modeling-javascript
 
-    categories: any = []
-    applets: any = []
-    selectedCategory: string = ''
+  categories: any = [];
+  applets: any = [];
+  selectedCategory: string = '';
 
-    getModel() {
-      let arr: any = []
-      this.lib.applets.forEach(x => {
-        x.categories.forEach(j => {
-          arr.push(j)
-        })
-      })
-      this.categories = uniq(arr)
-    }
+  getModel() {
+    let arr: any = [];
+    this.lib.applets.forEach((x) => {
+      x.categories.forEach((j) => {
+        arr.push(j);
+      });
+    });
+    this.categories = uniq(arr);
+  }
 
-    getApplets() {
-      this.lib.applets.forEach((x: any) => {
-        // console.log(x.categories, this.selectedCategory)
-        if (x.categories.includes(this.selectedCategory)) {
-          this.applets.push(x.name)
-        }
-        // console.log('applets :', this.applets)
-      })
-      
-    }
+  getApplets() {
+    this.lib.applets.forEach((x: any) => {
+      if (x.categories.includes(this.selectedCategory)) {
+        this.applets.push(x.name);
+      }
+    });
+  }
 
-    setCategory(category: string) {
-      this.applets = []
-      this.selectedCategory = category
-      // console.log(this.selectedCategory)
-      // if (this.selectedCategory === '') {
-      //   this.selectedCategory = this.categories[0]
-      //   console.log(this.categories)
-      // } else {
-      //   this.selectedCategory = category
-      //   console.log(this.selectedCategory)
-      // }
-    }
+  setCategory(category: string) {
+    this.applets = [];
+    this.selectedCategory = category;
+    // console.log(this.selectedCategory)
+    // if (this.selectedCategory === '') {
+    //   this.selectedCategory = this.categories[0]
+    //   console.log(this.categories)
+    // } else {
+    //   this.selectedCategory = category
+    //   console.log(this.selectedCategory)
+    // }
+  }
 
-    ngOnInit(): void {
-      this.getModel()
-      this.getApplets()
-      // let onLoad = this.categories[0].toString()
-      // console.log(onLoad)
-      // this.selectedCategory = onLoad
-    }
+  ngOnInit(): void {
+    this.getModel();
+    this.getApplets();
+    // let onLoad = this.categories[0].toString()
+    // console.log(onLoad)
+    // this.selectedCategory = onLoad
+  }
 }
